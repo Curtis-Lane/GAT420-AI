6d9900a94812840f3221b26b857bde069184114c
Curtis-Lane <129118489+Curtis-Lane@users.noreply.github.com>
2024-01-04 17:48:04 -0700

    Completed the Conway's Game of Life assignment

---
+++ Src/A-Life/GameOfLife.cpp
@@ -0,0 +1,89 @@
+#include "GameOfLife.h"
+
+#include "Texture.h"
+#include "Random.h"
+
+bool GameOfLife::Initialize() {
+	bufferA.resize(size.x * size.y);
+	bufferB.resize(size.x * size.y);
+
+	return true;
+}
+
+void GameOfLife::Step() {
+	frame += !paused ? 1 : 0;
+
+	bool evenFrame = (frame % 2 == 0) ? true : false;
+	std::vector<uint8_t>& readBuffer = evenFrame ? bufferA : bufferB;
+	std::vector<uint8_t>& writeBuffer = evenFrame ? bufferB : bufferA;
+
+	if(!paused) {
+		// Update buffer
+		// Clear write buffer
+		std::fill(writeBuffer.begin(), writeBuffer.end(), 0);
+
+		for(int y = 0; y < size.y; y++) {
+			for(int x = 0; x < size.x; x++) {
+				int weight = 0;
+
+				// Read surrounding 8 cells, accumulate weight
+				// XXX
+				// X0X
+				// XXX
+				weight += Read<uint8_t>(readBuffer, x - 1, y - 1);
+				weight += Read<uint8_t>(readBuffer, x, y - 1);
+				weight += Read<uint8_t>(readBuffer, x + 1, y - 1);
+				weight += Read<uint8_t>(readBuffer, x + 1, y);
+				weight += Read<uint8_t>(readBuffer, x + 1, y + 1);
+				weight += Read<uint8_t>(readBuffer, x, y + 1);
+				weight += Read<uint8_t>(readBuffer, x - 1, y + 1);
+				weight += Read<uint8_t>(readBuffer, x - 1, y);
+
+				// Game of life rules
+				// If cell is alive, update
+				if(Read<uint8_t>(readBuffer, x, y)) {
+					if(weight < 2) {
+						Write<uint8_t>(writeBuffer, x, y, 0);
+					} else if(weight > 3) {
+						Write<uint8_t>(writeBuffer, x, y, 0);
+					} else {
+						Write<uint8_t>(writeBuffer, x, y, 1);
+					}
+				} else {
+					// If cell is dead, update
+					if(weight == 3) {
+						Write<uint8_t>(writeBuffer, x, y, 1);
+					}
+				}
+			}
+		}
+	}
+
+	// "draw" onto buffer
+	if(mouse_button[0]) {
+		Write<uint8_t>(writeBuffer, mouse_position.x, mouse_position.y, 1);
+	} else if(mouse_button[2]) {
+		Write<uint8_t>(writeBuffer, mouse_position.x, mouse_position.y, 0);
+	}
+
+	// Convert buffer data format to color buffer
+	std::transform((writeBuffer).begin(), (writeBuffer).end(), color_buffer.begin(), [](uint8_t v) {
+		{return v ? 0xFFFFFFFF : 0;}
+	});
+
+	// Copy color buffer to texture
+	texture->Copy(color_buffer);
+}
+
+void GameOfLife::KeyDown(SDL_Keycode keycode) {
+	if(keycode == SDLK_r) {
+		// Write random 'alive' cells to buffer
+		std::vector<uint8_t>& writeBuffer = (frame % 2 == 1) ? bufferA : bufferB;
+		for(int i = 0; i < writeBuffer.size(); i++) {
+			writeBuffer[i] = (random(0, 10) == 0) ? 1 : 0;
+		}
+	}
+	if(keycode == SDLK_e) {
+		paused = !paused;
+	}
+}
--- Src/A-Life/Main.cpp
+++ Src/A-Life/Main.cpp
@@ -2,13 +2,21 @@
 #include <iostream>
 #include <memory>
 
-#include "CA.h"
+// Framerate cap
+#include <thread>
+#include <chrono>
+
+#include "GameOfLife.h"
 #include "Renderer.h"
 #include "Texture.h"
 
+const int FRAMERATE_CAP = 60;
+
 int main(int argc, char* argv[]) {
 	glm::ivec2 screen_size{800, 600};
-	glm::ivec2 env_size{400, 300};
+	glm::ivec2 env_size{200, 150};
+
+	int framerate = (FRAMERATE_CAP == 0) ? 0 : (1.0f / FRAMERATE_CAP) * 1000;
 
 	// create renderer
 	Renderer renderer;
@@ -19,7 +27,7 @@ int main(int argc, char* argv[]) {
 	std::shared_ptr<Texture> texture = std::make_unique<Texture>(env_size.x, env_size.y, renderer);
 
 	// create environment
-	std::unique_ptr<Environment> env = std::make_unique<CA>(env_size.x, env_size.y, texture);
+	std::unique_ptr<Environment> env = std::make_unique<GameOfLife>(env_size.x, env_size.y, texture);
 	env->Initialize();
 
 	bool quit = false;
@@ -39,16 +47,19 @@ int main(int argc, char* argv[]) {
 				env->MousePositionNormalized(glm::vec2{event.motion.x, event.motion.y} / (glm::vec2) screen_size);
 				break;
 			case SDL_MOUSEBUTTONDOWN:
-				env->MouseButtonDown(event.button.button);
+				env->MouseButtonDown(event.button.button - 1);
 				break;
 			case SDL_MOUSEBUTTONUP:
-				env->MouseButtonUp(event.button.button);
+				env->MouseButtonUp(event.button.button - 1);
 				break;
 		}
 
 		// update environment
 		env->Step();
 		texture->Copy(env->GetColorBuffer());
+
+		// Framerate cap
+		std::this_thread::sleep_for(std::chrono::milliseconds(framerate));
 
 		renderer.RenderCopy(texture);
 		renderer.Present();
